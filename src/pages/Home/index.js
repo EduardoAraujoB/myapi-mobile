8797aff3c4f8eca0b83be394c47e8ac0ee2d4615
unknown <ea0123450@gmail.com>
2019-06-30 15:36:24 -0300

    now articles are shown on home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,67 @@
 import React, { Component } from "react";
+import { FlatList } from "react-native";
 
-import { Container, Title } from "./styles";
+import api from "../../services/api";
+import {
+  Container,
+  ArticleContainer,
+  ArticleTitle,
+  ArticleAuthor,
+  ArticleButton,
+  Label
+} from "./styles";
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: []
+    };
+  }
+  _isMounted = false;
+  loading = false;
+
+  componentWillMount = async () => {
+    this._isMounted = true;
+    this.loading = true;
+    const articles = await api.get("/articles");
+    if (this._isMounted) {
+      this.loading = false;
+      console.log(articles.data);
+      this.setState({ articles: articles.data });
+    }
+  };
+
+  componentWillUnmount = () => {
+    this._isMounted = false;
+  };
+
   render() {
-    return (
-      <Container>
-        <Title>Home</Title>
-      </Container>
-    );
+    if (this.loading) {
+      return (
+        <Container>
+          <ArticleTitle>Carregando</ArticleTitle>
+        </Container>
+      );
+    } else {
+      return (
+        <Container>
+          <FlatList
+            data={this.state.articles}
+            keyExtractor={article => article._id}
+            renderItem={({ item }) => (
+              <ArticleContainer>
+                <ArticleTitle>{item.title}</ArticleTitle>
+                <ArticleAuthor>Autor: {item.member.name}</ArticleAuthor>
+                <ArticleButton activeOpacity={0.8}>
+                  <Label>Acessar</Label>
+                </ArticleButton>
+              </ArticleContainer>
+            )}
+          />
+        </Container>
+      );
+    }
   }
 }
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,13 +1,51 @@
-import styled from "styled-components";
+import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
+  padding: 30px 10px 30px;
+  background: #eee;
+  width: 100%;
+  height: 100%;
 `;
 
-export const Title = styled.Text`
+export const ArticleContainer = styled.View`
+  background: white;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 20px;
+  border-radius: 10px;
+  margin-bottom: 20px;
+  width: 300px;
+`;
+
+export const ArticleTitle = styled.Text`
   color: #fc6963;
   font-size: 20px;
   font-weight: bold;
+`;
+
+export const ArticleAuthor = styled.Text`
+  margin: 10px;
+  font-size: 15px;
+  font-weight: bold;
+  color: #5e5e5e;
+`;
+
+export const ArticleButton = styled.TouchableOpacity`
+  justify-content: center;
+  align-items: center;
+  background: #fc6963;
+  border-radius: 25px;
+  width: 100%;
+  height: 40px;
+`;
+
+export const Label = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  font-size: 15px;
 `;
