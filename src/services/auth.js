a712f85ac6d2092bbe11358f616b140f67c328f1
unknown <ea0123450@gmail.com>
2019-07-01 17:28:32 -0300

    starting to make authentication services

---
+++ src/services/auth.js
@@ -0,0 +1,32 @@
+import { AsyncStorage } from "react-native";
+import axios from "axios";
+
+export const isAutenticated = async () => {
+  const token = await getToken();
+  if (!token) {
+    return false;
+  } else {
+    const response = await axios.post(
+      "https://test-of-myapi.herokuapp.com/token/validate",
+      token
+    );
+    if (response.data.token === "valid") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+export const getToken = async () => {
+  const response = await AsyncStorage.getItem("token");
+  return response;
+};
+
+export const login = async token => {
+  await AsyncStorage.setItem("@MobileTes:token", token);
+};
+
+export const logout = async () => {
+  await AsyncStorage.removeItem("token");
+};
